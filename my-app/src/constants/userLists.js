e595828aa4026d7614c72198944fd8389d3b6b79
Igor Nogueira <igorcmnog@gmail.com>
2021-04-14 12:16:08 -0500

    adding users

--- my-app/src/constants/userLists.js
+++ my-app/src/constants/userLists.js
@@ -1,3 +1,10 @@
-const USERS_LIST = ['igor@samisaude.com']
+const USERS_LIST = [
+    'igor@samisaude.com',
+    'felipe@samisaude.com',
+    'gustavo.mello@samisaude.com',
+    'italo.augusto@samisaude.com',
+    'geovane.rocha@samisaude.com',
+    'nicollas.carvalho@samisaude.com'
+]
 
 export default USERS_LIST
