0aeeb452ff88f060278378207282c8b6e5e7975f
Igor Nogueira <igorcmnog@gmail.com>
2021-04-14 12:57:25 -0500

    adding users

--- my-app/src/constants/userLists.js
+++ my-app/src/constants/userLists.js
@@ -1,6 +1,7 @@
 const USERS_LIST = [
     'igor@samisaude.com',
     'felipe@samisaude.com',
+    'felipe.medeiros@samisaude.com',
     'gustavo.mello@samisaude.com',
     'italo.augusto@samisaude.com',
     'geovane.rocha@samisaude.com',
