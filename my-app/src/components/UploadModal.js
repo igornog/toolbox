003f463459fe6905590df0d57fc2eb7a2bcb9ef1
Nicollas Matheus <n.matheuscarvalho@gmail.com>
2021-04-26 09:03:10 -0300

    feat: first commit

--- my-app/src/components/UploadModal.js
+++ my-app/src/components/UploadModal.js
@@ -1,10 +1,74 @@
 import React from "react";
+
+import BeneficiaryService from '../services/beneficiary.service'
+import useState from '../hooks/useState'
+
 import Button from "../atoms/button";
 import "./uploadModal.scss";
 
-function UploadModal(params) {
+const  UploadModal = (props) => {
+  const [ state, setState ] = useState({})
+
   const closeUploadModal = () => {
-    params.setUploadModalOn(false);
+    props.setUploadModalOn(false);
+  };
+
+  const fileData = () => {
+    if (state.doc) {
+      return (
+        <p className="upload-details">
+          Arquivo adicionado: {state.doc.name}
+        </p>
+      );
+    } else {
+      return (
+        <p className="upload-details no-file">
+          Nenhum arquivo adicionado
+        </p>
+      );
+    }
+  };
+
+  const onFileChange = (event) => {
+    setState({ doc: event.target.files[0] });
+    const data = new FormData();
+    data.append("spreadsheet", event.target.files[0]);
+    const options = {
+      onUploadProgress: (progressEvent) => {
+        const { loaded, total } = progressEvent;
+        let percent = Math.floor((loaded * 100) / total);
+        
+
+        if (percent < 100) {
+          setState({ ...state, uploadPercentage: percent });
+        }
+      },
+    };
+    
+    BeneficiaryService.uploadSpreadsheet(
+      data,
+      props.hirerData.company._id,
+      options
+    )
+      .then((_) => {
+        setState({ ...state, uploadPercentage: 100 }, () => {
+          setTimeout(() => {
+            setState({
+              ...state,
+              uploadPercentage: 0,
+            });
+          }, 1000);
+        });
+      })
+      .catch((error) => {
+        console.log("error")
+      })
+      .finally(() => {
+        let companyId = props.hirerData.company._id;
+        if (companyId) {
+          props.fetchBeneficiaries(companyId)
+        };
+      })
   };
 
   return (
@@ -15,7 +79,17 @@ function UploadModal(params) {
           <span onClick={closeUploadModal}><p>X</p></span>
           <div className="content">
             <Button>Baixar planilha</Button>
-            <Button>Subir planilha</Button>
+            <Button onClick></Button>
+            <div className="input-wrap">
+              <input
+                type="file"
+                id="docUpload"
+                name="docUpload"
+                onChange={onFileChange}
+              />
+              <label for="docUpload">Subir planilha</label>
+              {fileData()}
+            </div>
           </div>
         </div>
       </section>
